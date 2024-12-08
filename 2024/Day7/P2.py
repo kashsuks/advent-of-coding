b50a2ff205f80fa5d3e61e9682837328ee47fe69
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2024-12-08 00:13:16 -0500

    added day 7

---
+++ 2024/Day7/P2.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+from pathlib import Path
+from operator import add, mul
+
+def concat(x, y): return int(str(x) + str(y))
+
+text = Path('input.txt').read_text('utf-8')
+
+def process(operators: list[callable]) -> int:
+    data = [
+        (int(data[0][:-1]), list(map(int, data[1:])))
+        for data in [line.split() for line in text.splitlines()]]
+
+    result = 0
+    operator_count = len(operators)
+
+    for target, values in data:
+        value_count = len(values)
+
+        for p in range(operator_count ** (value_count - 1)):
+            value = values[0]
+
+            for j in range(1, value_count):
+                p, r = divmod(p, operator_count)
+                value = operators[r](value, values[j])
+
+            if value == target:
+                result += target
+                break
+
+    return result
+
+print("Part 2:", process(operators=[add, mul, concat]))
